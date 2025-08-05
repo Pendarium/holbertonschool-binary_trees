8ee96bfc68dfae87a8fb239593795b27a5bf8fdd
Pendarium <thomas.sorli21@gmail.com>
2025-08-05 16:44:20 +0200

    uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,3 +8,33 @@
  */
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *grand_parent;
+
+	/* Si le nœud est NULL, il n’a pas d’oncle */
+	if (node == NULL)
+		return (NULL);
+
+	/* Si le nœud n’a pas de parent, il ne peut pas avoir d’oncle */
+	if (node->parent == NULL)
+		return (NULL);
+
+	/* Si le parent n’a pas de parent, il n’y */
+	/* a pas de grand-parent → pas d’oncle */
+	if (node->parent->parent == NULL)
+		return (NULL);
+
+	/* On récupère le grand-parent */
+	grand_parent = node->parent->parent;
+
+	/* Si le parent est l’enfant droit, l’oncle est l’enfant gauche */
+	if (node->parent == grand_parent->right)
+		return (grand_parent->left);
+
+	/* Si le parent est l’enfant gauche, l’oncle est l’enfant droit */
+	if (node->parent == grand_parent->left)
+		return (grand_parent->right);
+
+	/* Cas de sécurité, normalement jamais atteint */
+	return (NULL);
+}
